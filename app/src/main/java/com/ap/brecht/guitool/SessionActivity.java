0b6d712f3b67112c8e73223ede4184863f8654c0
ErazerBrecht <brecht.carlier@student.ap.be>
2015-04-30 13:57:59 +0200

    Forgot to delete something.

--- app/src/main/java/com/ap/brecht/guitool/SessionActivity.java
+++ app/src/main/java/com/ap/brecht/guitool/SessionActivity.java
@@ -81,11 +81,6 @@ public class SessionActivity extends ActionBarActivity implements ActionBar.TabL
     }
 
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
