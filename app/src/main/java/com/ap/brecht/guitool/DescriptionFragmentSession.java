859d2f7457ff1fafac30b7c93e77f7d8487bb436
Hannah <hannelore.hauquier@hotmail.com>
2015-05-06 17:38:53 +0200

    tryout text on image (not working)

--- app/src/main/java/com/ap/brecht/guitool/DescriptionFragmentSession.java
+++ app/src/main/java/com/ap/brecht/guitool/DescriptionFragmentSession.java
@@ -1,25 +1,90 @@
 package com.ap.brecht.guitool;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by hannelore on 22/04/2015.
  */
-public class DescriptionFragmentSession extends Fragment {
+public class DescriptionFragmentSession extends Fragment implements View.OnClickListener {
 
     private View view;
+    private Button SavePicButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_description_session, container, false);
 
+        SavePicButton = (Button) view.findViewById(R.id.savePicture);
+        SavePicButton.setOnClickListener(this);
+
         return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.savePicture:
+                Save();
+                break;
+        }
+    }
+
+    public void Save()
+    {
+        try {
+            File Drawn = new File(Environment.getExternalStorageDirectory().toString()+"/ClimbUP/");
+            Drawn.mkdirs();
+            File Drawing = File.createTempFile(
+                    "tryout",  /* prefix */
+                    ".jpg",         /* suffix */
+                    Drawn      /* directory */
+            );
+            FileOutputStream out = new FileOutputStream(Drawing);
+
+            // NEWLY ADDED CODE STARTS HERE [
+            Bitmap originalBitmap = BitmapFactory.decodeFile(SessionActivity.mCurrentPhotoPath2);
+            Canvas canvas = new Canvas(originalBitmap);
+
+            Paint paint = new Paint();
+            paint.setColor(Color.WHITE); // Text Color
+            paint.setStyle(Paint.Style.FILL);
+            paint.setStrokeWidth(16); // Text Size
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)); // Text Overlapping Pattern
+            // some more settings...
+
+            canvas.drawBitmap(originalBitmap, 0, 0, paint);
+            canvas.drawText("Testing...", 10, 10, paint);
+            // NEWLY ADDED CODE ENDS HERE ]
+
+            originalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
 
 
--- app/src/main/java/com/ap/brecht/guitool/SessionActivity.java
+++ app/src/main/java/com/ap/brecht/guitool/SessionActivity.java
@@ -4,6 +4,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -18,6 +25,7 @@ import android.view.MenuItem;
 import android.view.ViewConfiguration;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
@@ -41,6 +49,7 @@ public class SessionActivity extends ActionBarActivity implements ActionBar.TabL
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
+    static String mCurrentPhotoPath2;
     String mCurrentPhotoPath;
 
     @Override
@@ -73,6 +82,7 @@ public class SessionActivity extends ActionBarActivity implements ActionBar.TabL
             });
             pictureAlert.setCancelable(true);
             pictureAlert.create().show();
+
         }
 
 
@@ -195,11 +205,13 @@ public class SessionActivity extends ActionBarActivity implements ActionBar.TabL
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+
+
     }
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "JPEG_" + timeStamp;
         File storageDir = new File(Environment.getExternalStorageDirectory().toString()+"/ClimbUP/");
         storageDir.mkdirs();
         File image = File.createTempFile(
@@ -210,6 +222,9 @@ public class SessionActivity extends ActionBarActivity implements ActionBar.TabL
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        mCurrentPhotoPath2=image.getAbsolutePath();
         return image;
     }
+
+
 }
