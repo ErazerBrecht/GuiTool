798ec4188923139259e9d2150b2b5ad43da39273
Hannah <hannelore.hauquier@hotmail.com>
2015-05-06 14:59:48 +0200

    added xml files for description data and session added description session layout

--- app/src/main/java/com/ap/brecht/guitool/DescriptionFragmentSession.java
+++ app/src/main/java/com/ap/brecht/guitool/DescriptionFragmentSession.java
@@ -1,9 +1,25 @@
 package com.ap.brecht.guitool;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by hannelore on 22/04/2015.
  */
 public class DescriptionFragmentSession extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_description_session, container, false);
+
+        return view;
+    }
 }
+
+
