58c9fee2d454db97c6b7eec2ca2c9b13eac100e2
Airien1992 <witeengel@hotmail.com>
2015-05-08 20:51:47 +0200

    The history fragment working

--- app/src/main/java/com/ap/brecht/guitool/GraphFragmentData.java
+++ app/src/main/java/com/ap/brecht/guitool/GraphFragmentData.java
@@ -1,23 +1,28 @@
 package com.ap.brecht.guitool;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Random;
+
 /**
  * Created by Airien on 29/04/2015.
  */
-public class GraphFragmentData extends Fragment implements View.OnClickListener {
+public class GraphFragmentData extends Fragment {
 
     private View view;
 
+    Button btnRandom;
     GraphView graphView;
     LineGraphSeries<DataPoint> series;
     DataPoint newDataPoint;
@@ -26,7 +31,19 @@ public class GraphFragmentData extends Fragment implements View.OnClickListener 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_graph_data, container, false);
-        graphView = (GraphView) view.findViewById(R.id.graph);
+        btnRandom = (Button) view.findViewById(R.id.btnRandom1);
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnRandom1:
+                        AddRandomNumber();
+                        break;
+                    default:
+                        break;
+            }}
+        });
+        graphView = (GraphView) view.findViewById(R.id.graph1);
         oldDataPoint = new DataPoint(0, 0);
         series = new LineGraphSeries<DataPoint>(new DataPoint[]{
                 oldDataPoint
@@ -46,14 +63,32 @@ public class GraphFragmentData extends Fragment implements View.OnClickListener 
         return view;
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            default:
-                break;
-        }
+
+    private void AddRandomNumber() {
+        DataPoint newDataPoint = new DataPoint(randInt(-20, 20), randInt(0, 40));
+        series = new LineGraphSeries<DataPoint>(new DataPoint[]
+                {
+                        oldDataPoint,
+                        newDataPoint
+                });
+
+        int color = Color.argb(255, randInt(100, 255), randInt(100, 255), randInt(100, 255));
+        series.setColor(color);
+        graphView.addSeries(series);
+        oldDataPoint = newDataPoint;
     }
 
-}
+    private static int randInt(int min, int max) {
 
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+}
 
--- app/src/main/java/com/ap/brecht/guitool/HistoryActivity.java
+++ app/src/main/java/com/ap/brecht/guitool/HistoryActivity.java
@@ -13,16 +13,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Airien on 29/04/2015.
  */
 public class HistoryActivity extends ActionBarActivity {
     ActionBar actionbar;
+    String tussenResultaat;
     ListView list;
+    ArrayList<String> list1;
+    ArrayAdapter<String> adapter;
+    JSONArray a = null;
+    JSONObject o=null;
 
     public static final String TAG =HistoryActivity.class.getSimpleName();
 
@@ -31,20 +44,58 @@ public class HistoryActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_history);
-
         list= (ListView) findViewById(R.id.historyData);
 
-        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        list1=new ArrayList<>();
+
+
+        try {
+            a = DatabaseData.userData.getJSONArray("session");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        int arrSize = a.length();
+        List<String> datum = new ArrayList<String>(arrSize);
+        List<String> plaats = new ArrayList<String>(arrSize);
+        for (int i = 0; i < arrSize; ++i) {
+            try {
+                o=a.getJSONObject(i);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                datum.clear();
+                datum.add(o.getString("datum").toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                plaats.clear();
+                plaats.add(o.getString("place").toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            tussenResultaat=String.valueOf(datum).replace('[',' ').replace(']',' ').trim() + "\n" + String.valueOf(plaats).replace('[',' ').replace(']',' ').trim();
+            list1.add(tussenResultaat);
+        }
+        adapter=new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item,list1);
+        list.setAdapter(adapter);
+
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(HistoryActivity.this, SessionActivity.class);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(HistoryActivity.this, HistoryDataActivity.class);
+                try {
+                    i.putExtra("sid",o.getString("sid").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 HistoryActivity.this.startActivity(i);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
