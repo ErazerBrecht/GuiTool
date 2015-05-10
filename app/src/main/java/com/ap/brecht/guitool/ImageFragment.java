b8187a6c9cddaee8ff93bedb9e79d104fcbf219b
Hannah <hannelore.hauquier@hotmail.com>
2015-05-10 15:25:07 +0200

    added picture to historydata

--- app/src/main/java/com/ap/brecht/guitool/ImageFragment.java
+++ app/src/main/java/com/ap/brecht/guitool/ImageFragment.java
@@ -1,21 +1,58 @@
 package com.ap.brecht.guitool;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by hannelore on 9/05/2015.
  */
 public class ImageFragment extends Fragment {
     private View view;
+    private ImageView Picture;
+
+    JSONArray a = null;
+    JSONObject o = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_image, container, false);
+
+        Picture = (ImageView) view.findViewById(R.id.Picture);
+
+        try {
+            a = DatabaseData.userData.getJSONArray("session");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        int arrSize = a.length();
+        for (int i = 0; i < arrSize; ++i) {
+            try {
+                o = a.getJSONObject(i);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (o.getString("sid").equals(DatabaseData.Sid)) {
+                    Bitmap myBitmap = BitmapFactory.decodeFile(o.getString("path"));
+                    Picture.setImageBitmap(myBitmap);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
         return view;
     }
